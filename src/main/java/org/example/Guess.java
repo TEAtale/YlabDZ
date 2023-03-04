b543c1b0cb2b9e728164bfe4993c6d4a98d565f2
TEAtale <ev00genia@gmail.com>
2023-03-04 10:42:40 +0400

    firstdz

---
+++ src/main/java/org/example/Guess.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess {
+    public static void main(String[] args) {
+        int number = new Random().nextInt(99) + 1; // здесь загадывается число от 1 до 99
+        int maxAttempts = 10; // здесь задается количество попыток
+        System.out.println("Я загадал число. У тебя " + maxAttempts + " попыток угадать.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int tries = 1;
+
+            while (maxAttempts > 0) {
+                if (n == number) {
+                    System.out.println("Ты угадал с " + tries + " попытки");
+                    break;
+                } else if (n < number) {
+                    maxAttempts--;
+                    System.out.println("Мое число больше! осталось " + maxAttempts + " попыток");
+                    if (maxAttempts == 0) {
+                        System.out.println("Ты не угадал!");
+                        break;
+                    }
+                    tries++;
+                    n = scanner.nextInt();
+                } else {
+                    maxAttempts--;
+                    System.out.println("Мое число меньше! осталось " + maxAttempts + " попыток");
+                    if (maxAttempts == 0) {
+                        System.out.println("Ты не угадал!");
+                        break;
+                    }
+                    tries++;
+                    n = scanner.nextInt();
+                }
+
+            }
+        }
+
+    }
+
+}
+
---
+++ src/main/java/org/example/Pell.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Pell {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int a = 1;
+            int b = 0;
+            if (n < 3) {
+                System.out.println(n);
+            }
+            else {
+                for (int i = 2; i <= n; i++) {
+                   int temp = 2*a + b;
+                   b = a;
+                   a = temp;
+                }
+                System.out.println(a);
+            }
+
+        }
+
+    }
+}
