e18ca7ec2eaf7f792c83b1c3dbacbcd61db5f9d0
TEAtale <ev00genia@gmail.com>
2023-03-06 00:08:31 +0400

    firstLesson

--- src/main/java/org/example/Guess.java
+++ src/main/java/org/example/Guess.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Guess {
     public static void main(String[] args) {
         int number = new Random().nextInt(99) + 1; // здесь загадывается число от 1 до 99
-        int maxAttempts = 9; // здесь задается количество попыток
-        System.out.println("Я загадал число. У тебя " + (maxAttempts + 1) + " попыток угадать.");
+        int maxAttempts = 10; // здесь задается количество попыток
+        System.out.println("Я загадал число. У тебя " + maxAttempts + " попыток угадать.");
         try (Scanner scanner = new Scanner(System.in)) {
             int n = scanner.nextInt();
             int tries = 1;
@@ -16,18 +16,19 @@ public class Guess {
                 if (n == number) {
                     System.out.println("Ты угадал с " + tries + " попытки");
                     break;
-                } else if (n < number) {
-                    System.out.println("Мое число больше! осталось " + maxAttempts + " попыток");
-
                 } else {
-                    System.out.println("Мое число меньше! осталось " + maxAttempts + " попыток");
+                    maxAttempts--;
+                    if (n < number) {
+                        System.out.println("Мое число больше! осталось " + maxAttempts + " попыток");
+                    } else {
+                        System.out.println("Мое число меньше! осталось " + maxAttempts + " попыток");
                     }
+                }
                    if (maxAttempts == 0) {
                     System.out.println("Ты не угадал!");
                     break;
                 }
                 tries++;
-                maxAttempts--;
                 n = scanner.nextInt();
             }
         }
